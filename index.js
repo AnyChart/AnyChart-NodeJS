df7513ff10101c3e340f4b78518e756390e68aab
blackart <blackart.ru@gmail.com>
2016-12-07 17:36:16 +0800

    text measuring

--- index.js
+++ index.js
@@ -1,138 +1,126 @@
 var fs = require('fs');
 var jsdom = require('jsdom');
+var opentype = require('opentype.js');
 // window = jsdom.jsdom().defaultView;
 // document = window.document;
 var spawn = require('child_process').spawn;
+var loadedFonts = {};
+var font;
 
 // var any = fs.readFileSync("../ACDVF/out/anychart-bundle.min", "utf-8");
 // var anychart = require('../ACDVF/out/anychart-bundle.min');
-
-jsdom.env(
-    '<html><head></head><body><div id="container" style="width: 800px; height: 600px;"></div></body></html>',
-    // ["https://cdn.anychart.com/js/7.12.0/anychart-bundle.min.js"],
-    ["../ACDVF/out/anychart-bundle.min.js"],
-    function(errors, window) {
-      var $ = require("jquery")(window);
-      var chart, svg, convert, buffer;
-
-
-      // this.measurement_ex = goog.dom.createDom(goog.dom.TagName.DIV);
-      // this.virtualBaseLine_ = goog.dom.createDom(goog.dom.TagName.SPAN);
-      // this.measurementText_ex = goog.dom.createDom(goog.dom.TagName.SPAN);
-      //
-      // goog.style.setStyle(this.measurement_ex, {'position': 'absolute', 'visibility': 'hidden', 'left': 0, 'top': 0});
-      // goog.style.setStyle(this.measurementText_ex, {'font-size': '0px', 'border': '0 solid'});
-      // this.measurementText_ex.innerHTML = 'a';
-      // goog.style.setStyle(this.virtualBaseLine_, {'font-size': '0px', 'border': '0 solid'});
-      // this.virtualBaseLine_.innerHTML = 'a';
-      //
-      // goog.dom.appendChild(document.body, this.measurement_ex);
-      // goog.dom.appendChild(this.measurement_ex, this.virtualBaseLine_);
-      // goog.dom.appendChild(this.measurement_ex, this.measurementText_ex);
+function anychartify(window) {
+  window.document.createElementNS = function(ns, tagName) {
+    var elem = window.document.createElement(tagName);
+    elem.getBBox = function() {
+      var text = window.$(elem).text();
+      var fontSize = elem.getAttribute('font-size');
+      var fontFamily = elem.getAttribute('font-family');
+      var fonts = fontFamily.split(', ');
+      var font;
+      for (var i = 0, len = fonts.length; i < len; i++) {
+        if (font = loadedFonts[fonts[i]])
+          break;
+      }
+      if (!font)
+        font = loadedFonts['verdana'];
 
 
-      var measure_el = $('<div/>');
-      var baseLine_el = $('<span/>');
-      var measure_text_el = $('<span/>');
+      var scale = 1 / font.unitsPerEm * fontSize;
 
-      measure_el.css({'position': 'absolute', 'left': 0, 'top': 0});
-      measure_text_el.css({'border': '0 solid'});
-      baseLine_el.css({'border': '0 solid'});
+      var top = -font.ascender * scale;
+      var height = Math.abs(top) + Math.abs(font.descender * scale);
 
-      measure_text_el.html('a');
-      baseLine_el.html('a');
+      var width = 0;
 
-      $('body').append(measure_el);
-      measure_el.append(baseLine_el);
-      measure_el.append(measure_text_el);
+      font.forEachGlyph(text, 0, 0, fontSize, undefined, function(glyph, x, y, fontSize, options) {
+        var metrics = glyph.getMetrics();
+        metrics.xMin *= scale;
+        metrics.xMax *= scale;
+        metrics.leftSideBearing *= scale;
+        metrics.rightSideBearing *= scale;
 
-
-
-
-
-      measure_el.css({'left': 0, 'top': 0, 'width': 'auto', 'height': 'auto'});
-      // measure_text_el.css({
-      //   'border': '0 solid',
-      //   'position': 'absolute',
-      //   'left': 0,
-      //   'top': 0
-      // });
-
-      measure_text_el.html('sdfsdfs');
-
-      // var boundsMicroText = goog.style.getBounds(this.virtualBaseLine_);
-      // goog.style.setPosition(this.measurement_ex, 0, -(boundsMicroText.top + boundsMicroText.height));
-
-      // var boundsTargetText = goog.style.getBounds(this.measurementText_ex);
-      // this.measurementText_ex.innerHTML = '';
-
-      // return boundsTargetText;
-      //
-
-      // var b = baseLine_el.getBoundingClientRect();
-      console.log(measure_text_el[0].getClientRects());
-
-
-
-
-
-
-
-
-      document = window.document;
-      var anychart = window.anychart;
-
-      anychart.onDocumentReady(function() {
-
-        chart = anychart.column([1, 2, 3, 4, 5]);
-        chart.bounds(0, 0, 800, 600);
-        chart.container('container').draw();
-
-        var $container = $('#container svg')[0];
-        svg = $container.outerHTML;
-
-
-        var renderer = anychart.graphics.getRenderer();
-        console.log(renderer.measuringHTMLText('dsf', {}));
-
-        fs.writeFile('gg.svg', svg, function() {
-
-        });
-
-
-
-        // // Start convert
-        // convert = spawn('convert', ['svg:-', 'png:-']);
-        //
-        // // Pump in the svg content
-        // convert.stdin.write(svg);
-        // convert.stdin.end();
-        //
-        // // Write the output of convert straight to the response
-        // convert.stdout.on('data', function(data) {
-        //   try {
-        //     var prevBufferLength = (buffer ? buffer.length : 0),
-        //         newBuffer = new Buffer(prevBufferLength + data.length);
-        //
-        //     if (buffer) {
-        //       buffer.copy(newBuffer, 0, 0);
-        //     }
-        //
-        //     data.copy(newBuffer, prevBufferLength, 0);
-        //
-        //     buffer = newBuffer;
-        //   } catch (e) {
-        //     console.log(e);
-        //   }
-        // });
-        //
-        // convert.on('exit', function(code) {
-        //   console.log(code, buffer);
-        //   fs.writeFile('chart.png', buffer, function() {
-        //     console.log('Written to chart.png');
-        //     process.exit(0);
-        //   });
-        // });
+        width += Math.abs(metrics.xMax - metrics.xMin) + metrics.leftSideBearing + metrics.rightSideBearing
       });
-    }
-);
+
+      return {x: 0, y: top,  width: width, height: height};
+    };
+    return elem;
+  };
+}
+
+
+opentype.load('/Library/Fonts/Verdana.ttf', function(err, font_) {
+  if (err) {
+    alert('Could not load font: ' + err);
+  } else {
+    font = font_;
+    jsdom.env(
+        '<div id="container"></div>',
+        // ["https://cdn.anychart.com/js/7.12.0/anychart-bundle.min.js"],
+        ["../ACDVF/out/anychart-bundle.min.js"],
+        function(errors, window) {
+          var $ = require("jquery")(window);
+          var chart, svg, convert, buffer;
+
+          try {
+            document = window.document;
+
+            anychartify(window);
+
+            var anychart = window.anychart;
+
+            chart = anychart.column([1, 2, 3, 4, 5]);
+            chart.title('canvg is a SVG parser and renderer. It takes a URL to a SVG file or the text of an SVG file, parses it in JavaScript, and renders the result on a Canvas element. The rendering speed of the examples is about as fast as native SVG.');
+            chart.bounds(0, 0, 800, 600);
+            chart.container('container').draw();
+
+            var $container = $('#container svg')[0];
+            svg = $container.outerHTML;
+
+
+            fs.writeFile('gg.svg', svg, function() {
+              console.log('Written to gg.svg');
+            });
+
+            // Start convert
+            convert = spawn('convert', ['svg:-', 'png:-']);
+
+            // Pump in the svg content
+            convert.stdin.write(svg);
+            convert.stdin.end();
+
+            // Write the output of convert straight to the response
+            convert.stdout.on('data', function(data) {
+              try {
+                var prevBufferLength = (buffer ? buffer.length : 0),
+                    newBuffer = new Buffer(prevBufferLength + data.length);
+
+                if (buffer) {
+                  buffer.copy(newBuffer, 0, 0);
+                }
+
+                data.copy(newBuffer, prevBufferLength, 0);
+
+                buffer = newBuffer;
+              } catch (e) {
+                console.log(e);
+              }
+            });
+
+            convert.on('exit', function(code) {
+              fs.writeFile('chart.png', buffer, function() {
+                console.log('Written to chart.png');
+                process.exit(0);
+              });
+            });
+          } catch(e) {
+            console.log(e);
+            process.exit(0);
+          }
+        }
+    );
+  }
+});
+
+
