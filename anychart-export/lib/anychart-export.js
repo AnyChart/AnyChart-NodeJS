68891e864f11aed7e5fc63048c59c06bd1940523
blackart <blackart.ru@gmail.com>
2016-12-09 18:43:08 +0800

    work commit

--- anychart-export/lib/anychart-export.js
+++ anychart-export/lib/anychart-export.js
@@ -1,28 +1,13 @@
 // var jsdom = require('jsdom').jsdom;
-var jsdom = require('jsdom');
+var jsdom = require('jsdom').jsdom;
 var opentype = require('opentype.js');
 var spawn = require('child_process').spawn;
 
-// document = jsdom('<div id="container"></div>');
-// window = document.defaultView;
-//
-// window.addEventListener("load", function (event) {
-  var anychart = require('anychart');
-//
-  console.log(anychart);
-// });
+document = jsdom('<div id="container"></div>');
+window = document.defaultView;
 
+var anychart = require('anychart');
 
-// jsdom.env(
-//     '<div id="container"></div>',
-//     [
-//       // "https://cdn.anychart.com/js/7.12.0/anychart-bundle.min.js",
-//       "../ACDVF/out/anychart-bundle.min.js",
-//       "http://cdn.anychart.com/geodata/1.2.0/countries/united_states_of_america/united_states_of_america.js"
-//     ],
-//     function(errors, window) {
-//
-//     }
-// );
+console.log(anychart);
 
 
--- index.js
+++ index.js
@@ -4,27 +4,31 @@ var opentype = require('opentype.js');
 // window = jsdom.jsdom().defaultView;
 // document = window.document;
 var spawn = require('child_process').spawn;
-var loadedFonts = {};
-var font;
-var anychart = require("../ACDVF/out/anychart-bundle.min.js").anychart;
 
+var font;
+var anychart;
+var fonts = {};
+
+// var anychart = require("../ACDVF/out/anychart-bundle.min.js").anychart;
 // var any = fs.readFileSync("../ACDVF/out/anychart-bundle.min", "utf-8");
 // var anychart = require('../ACDVF/out/anychart-bundle.min');
-function anychartify(window) {
+
+function anychartify(window, document) {
   window.document.createElementNS = function(ns, tagName) {
     var elem = window.document.createElement(tagName);
     elem.getBBox = function() {
       var text = window.$(elem).text();
       var fontSize = parseFloat(elem.getAttribute('font-size'));
       var fontFamily = elem.getAttribute('font-family');
-      var fonts = fontFamily.split(', ');
-      // var font;
-      // for (var i = 0, len = fonts.length; i < len; i++) {
-      //   if (font = loadedFonts[fonts[i]])
-      //     break;
-      // }
-      // if (!font)
-      //   font = loadedFonts['verdana'];
+      var fontsArr = fontFamily.split(', ');
+
+      var font;
+      for (var i = 0, len = fontsArr.length; i < len; i++) {
+        if (font = fonts[fontsArr[i]])
+          break;
+      }
+      if (!font)
+        font = fonts['Verdana'];
 
 
       var scale = 1 / font.unitsPerEm * fontSize;
@@ -44,7 +48,7 @@ function anychartify(window) {
         width += Math.abs(metrics.xMax - metrics.xMin) + metrics.leftSideBearing + metrics.rightSideBearing
       });
 
-      console.log({x: 0, y: top,  width: width, height: height});
+      // console.log({x: 0, y: top,  width: width, height: height});
 
       return {x: 0, y: top,  width: width, height: height};
     };
@@ -53,7 +57,7 @@ function anychartify(window) {
 }
 
 
-function drawAnyChart(window) {
+function drawAnyChart() {
   var chart = anychart.column([1, 2, 3, 4, 5]);
   chart.a11y(false);
   chart.title('canvg is a SVG parser and renderer. It takes a URL to a SVG file or the text of an SVG file, parses it in JavaScript, and renders the result on a Canvas element. The rendering speed of the examples is about as fast as native SVG.');
@@ -62,198 +66,85 @@ function drawAnyChart(window) {
 }
 
 
-function drawHighchart(window) {
-  var Highcharts = require('highcharts');
+function loadDefaultFonts() {
+  var fontFilesList = fs.readdirSync('./fonts');
 
-// Load module after Highcharts is loaded
-  require('highcharts/modules/exporting')(Highcharts);
-
-// Create the chart
-  Highcharts.chart('container', {
-    chart: {
-      width: 1000,
-      height: 300
-    },
-    title: {
-      text: 'Monthly Average Temperature',
-      x: -20 //center
-    },
-    subtitle: {
-      text: 'Source: WorldClimate.com',
-      x: -20
-    },
-    xAxis: {
-      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    },
-    yAxis: {
-      title: {
-        text: 'Temperature (°C)'
-      },
-      plotLines: [{
-        value: 0,
-        width: 1,
-        color: '#808080'
-      }]
-    },
-    tooltip: {
-      valueSuffix: '°C'
-    },
-    legend: {
-      layout: 'vertical',
-      align: 'right',
-      verticalAlign: 'middle',
-      borderWidth: 0
-    },
-    series: [{
-      name: 'Tokyo',
-      data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-    }, {
-      name: 'New York',
-      data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-    }, {
-      name: 'Berlin',
-      data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-    }, {
-      name: 'London',
-      data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-    }]
-
-  });
+  for (var i = 0, len = fontFilesList.length; i < len; i++) {
+    var fileName = fontFilesList[i];
+    var fontName = fileName.split('.')[0];
+    if (!fonts[fontName]) {
+      fonts[fontName] = opentype.loadSync('./fonts/' + fileName);
+    }
+  }
 }
 
+loadDefaultFonts();
 
-opentype.load('/Library/Fonts/Verdana.ttf', function(err, font_) {
-  if (err) {
-    alert('Could not load font: ' + err);
-  } else {
-    font = font_;
-    jsdom.env(
-        '<div id="container"></div>',
-        [
-          // "https://cdn.anychart.com/js/7.12.0/anychart-bundle.min.js",
-          "http://cdn.anychart.com/geodata/1.2.0/countries/united_states_of_america/united_states_of_america.js"
-        ],
-        function(errors, window) {
-          var $ = require("jquery")(window);
-          var svg, convert, buffer;
+jsdom.env(
+    '<div id="container"></div>',
+    [
+      "../ACDVF/out/anychart-bundle.min.js"
+      // "https://cdn.anychart.com/js/7.12.0/anychart-bundle.min.js",
+      // "http://cdn.anychart.com/geodata/1.2.0/countries/united_states_of_america/united_states_of_america.js"
+    ],
+    function(errors, window) {
+      var $ = require("jquery")(window);
+      var svg, convert, buffer;
 
+      try {
+        document = window.document;
+        // require("../ACDVF/out/anychart-bundle.min.js");
+        anychart = window.anychart;
+
+
+        anychartify(window);
+        drawAnyChart();
+
+
+        var $container = $('#container svg')[0];
+        svg = $container.outerHTML;
+
+
+        fs.writeFile('gg.svg', svg, function() {
+          console.log('Written to gg.svg');
+        });
+
+        // Start convert
+        convert = spawn('convert', ['svg:-', 'png:-']);
+
+        // Pump in the svg content
+        convert.stdin.write(svg);
+        convert.stdin.end();
+
+        // Write the output of convert straight to the response
+        convert.stdout.on('data', function(data) {
           try {
-            document = window.document;
+            var prevBufferLength = (buffer ? buffer.length : 0),
+                newBuffer = new Buffer(prevBufferLength + data.length);
 
-            anychartify(window);
+            if (buffer) {
+              buffer.copy(newBuffer, 0, 0);
+            }
 
-            // var anychart = window.anychart;
+            data.copy(newBuffer, prevBufferLength, 0);
 
-            // drawAnyChart(window);
-
-            var Highcharts = require('highcharts');
-
-// Load module after Highcharts is loaded
-            require('highcharts/modules/exporting')(Highcharts);
-
-// Create the chart
-            Highcharts.chart('container', {
-              chart: {
-                width: 1000,
-                height: 300
-              },
-              title: {
-                text: 'Monthly Average Temperature',
-                x: -20 //center
-              },
-              subtitle: {
-                text: 'Source: WorldClimate.com',
-                x: -20
-              },
-              xAxis: {
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                  'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-              },
-              yAxis: {
-                title: {
-                  text: 'Temperature (°C)'
-                },
-                plotLines: [{
-                  value: 0,
-                  width: 1,
-                  color: '#808080'
-                }]
-              },
-              tooltip: {
-                valueSuffix: '°C'
-              },
-              legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'middle',
-                borderWidth: 0
-              },
-              series: [{
-                name: 'Tokyo',
-                data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-              }, {
-                name: 'New York',
-                data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-              }, {
-                name: 'Berlin',
-                data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-              }, {
-                name: 'London',
-                data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-              }]
-
-            });
-
-
-
-
-            var $container = $('#container svg')[0];
-            svg = $container.outerHTML;
-
-
-            fs.writeFile('gg.svg', svg, function() {
-              console.log('Written to gg.svg');
-            });
-
-            // Start convert
-            convert = spawn('convert', ['svg:-', 'png:-']);
-
-            // Pump in the svg content
-            convert.stdin.write(svg);
-            convert.stdin.end();
-
-            // Write the output of convert straight to the response
-            convert.stdout.on('data', function(data) {
-              try {
-                var prevBufferLength = (buffer ? buffer.length : 0),
-                    newBuffer = new Buffer(prevBufferLength + data.length);
-
-                if (buffer) {
-                  buffer.copy(newBuffer, 0, 0);
-                }
-
-                data.copy(newBuffer, prevBufferLength, 0);
-
-                buffer = newBuffer;
-              } catch (e) {
-                console.log(e);
-              }
-            });
-
-            convert.on('exit', function(code) {
-              fs.writeFile('chart.png', buffer, function() {
-                console.log('Written to chart.png');
-                process.exit(0);
-              });
-            });
-          } catch(e) {
+            buffer = newBuffer;
+          } catch (e) {
             console.log(e);
-            process.exit(0);
           }
-        }
-    );
-  }
-});
+        });
+
+        convert.on('exit', function(code) {
+          fs.writeFile('chart.png', buffer, function() {
+            console.log('Written to chart.png');
+            process.exit(0);
+          });
+        });
+      } catch (e) {
+        console.log(e);
+        process.exit(0);
+      }
+    }
+);
 
 
