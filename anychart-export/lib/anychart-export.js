169d6753d88a97bb287d54bd82a509cc27d8e9bd
blackart <blackart.ru@gmail.com>
2016-12-12 17:51:08 +0800

    work commit

--- anychart-export/lib/anychart-export.js
+++ anychart-export/lib/anychart-export.js
@@ -2,8 +2,16 @@ var fs = require('fs');
 // var jsdom = require('jsdom').jsdom;
 var opentype = require('opentype.js');
 var spawnSync = require('child_process').spawnSync;
+var extend = require('util')._extend;
+
 var fonts = {};
 var document = window.document;
+var defaultChartBounds = {
+  left: 0,
+  top: 0,
+  width: 1024,
+  height: 758
+};
 // var im = require('imagemagick');
 // var Stream = require('stream').Stream;
 var defaultImageSettings = [
@@ -124,16 +132,6 @@ function anychartify() {
 
 anychartify();
 
-function drawAnyChart() {
-  var chart = anychart.column([1, 2, 3, 4, 5]);
-  chart.a11y(false);
-  chart.title('canvg is a SVG parser and renderer. It takes a URL to a SVG file or the text of an SVG file, parses it in JavaScript, and renders the result on a Canvas element. The rendering speed of the examples is about as fast as native SVG.');
-  chart.bounds(0, 0, 800, 600);
-  chart.container('container').draw();
-}
-
-// drawAnyChart();
-
 function exportToPng_() {
   var $container = $('#container svg')[0];
   var svg = $container.outerHTML;
@@ -185,25 +183,26 @@ function getParams(args) {
   var lastArg = args[arrLength - 1];
   var callback = isFunction(lastArg) ? lastArg : null;
 
-  var endIndex = callback ? arrLength - 2 : arrLength - 1;
-  var optionsArr = Array.prototype.slice.call(args, 1, endIndex);
-  var options = {};
+  var options = arrLength == 1 ? undefined : callback ? args[arrLength - 2] : lastArg;
+  var params = {};
 
-  defaultImageSettings.forEach(function(item, index) {
-    var option = optionsArr[index];
-    options[item.name] = option === void 0 || option === null ? item.value : option;
-  });
+  extend(params, defaultImageSettings);
+  if (typeof options == 'string') {
+    params.type = options;
+  } else if (typeof options == 'object') {
+    extend(params, options)
+  }
 
-  return options;
+  return params;
 }
 
 function getSvg(target, params) {
-  var bounds = target.bounds();
-  target.bounds(bounds.left(), bounds.top(), params.width, params.height);
-  target.container('container').draw();
+  // var bounds = target.bounds();
+  // target.bounds(bounds.left(), bounds.top(), params.width, params.height);
+  // target.container('container').draw();
 
-  // var $container = $('#container svg')[0];
-  var svgElement = document.getElementsByTagName('svg')[0];
+  var $container = $(chart.container())[0];
+  // var svgElement = document.getElementsByTagName('svg')[0];
   var svg = svgElement.outerHTML;
   return svg;
 }
@@ -249,14 +248,19 @@ function convertSvgToImageData(svg, params) {
 }
 
 function exportTo(target, options, callback) {
-  var buffer = exportToSync(target, options);
-  callback.call(null, buffer);
+  setTimeout(function() {
+    var buffer = exportToSync(target, options);
+    if (callback) {
+      callback.call(null, buffer);
+    }
+  }, 0);
 }
 
 function exportToSync(target, options) {
   var params = getParams(arguments);
   var svg = getSvg(target, params);
-  return convertSvgToImageData(svg, params);
+
+  return params.type == 'svg' ? svg : convertSvgToImageData(svg, params);
 }
 
 function loadFont(path, callback) {
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var jsdom = require('jsdom').jsdom;
+// var spawnSync = require('child_process').spawnSync;
 
 var document = jsdom('<div id="container"></div>');
 var window = document.defaultView;
@@ -7,10 +8,10 @@ var window = document.defaultView;
 var anychart = require('anychart')(window);
 var anychart_export = require('./anychart-export/lib/anychart-export.js');
 
-var chart = anychart.column([1, 2, 3, 4, 5]);
-chart.title('canvg is a SVG parser and renderer. It takes a URL to a SVG file or the text of an SVG file, parses it in JavaScript, and renders the result on a Canvas element. The rendering speed of the examples is about as fast as native SVG.');
-chart.bounds(0, 0, 500, 600);
-chart.container('container').draw();
+// var chart = anychart.column([1, 2, 3, 4, 5]);
+// chart.title('canvg is a SVG parser and renderer. It takes a URL to a SVG file or the text of an SVG file, parses it in JavaScript, and renders the result on a Canvas element. The rendering speed of the examples is about as fast as native SVG.');
+// chart.bounds(0, 0, 500, 600);
+// chart.container('container').draw();
 
 // anychart_export.exportTo(chart, null, function(image) {
 //   fs.writeFile('./image.png', image, function() {
@@ -20,15 +21,51 @@ chart.container('container').draw();
 // });
 
 var options = {
-  type: 'jpg',
-  width: 800,
-  height: 600
+  type: 'svg',
+  width: 1000,
+  height: 500
 };
 
-var image = anychart_export.exportToSync(chart, options);
+// var svg = anychart_export.exportToSync(chart, 'svg');
+// console.log(svg);
 
-fs.writeFile('./image.' + options.type, image, function() {
-  console.log('Written to file');
-  process.exit(0);
-});
+// var image = anychart_export.exportToSync(chart, options);
+//
+// fs.writeFile('./image.' + options.type, image, function(err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log('Written to file');
+//   }
+//   // spawnSync('convert',
+//   //     ['-', '-resize "200%"', 'bigchart.jpg'],
+//   //     {
+//   //       input: image
+//   //     });
+//   // process.exit(0);
+// });
+
+var startDate = new Date().getTime() / 1000;
+for (var i = 0, len = 1; i < len; i++) {
+  var chart = anychart.column([1, 2, 3, 4, 5]);
+  chart.title('canvg is a SVG parser and renderer. It takes a URL to a SVG file or the text of an SVG file, parses it in JavaScript, and renders the result on a Canvas element. The rendering speed of the examples is about as fast as native SVG.');
+  chart.bounds(0, 0, 500, 600);
+  chart.container('container').draw();
+
+  console.log(chart.container())
+
+  chart.listen(anychart.enums.EventType.CHART_DRAW, function(e) {
+
+    anychart_export.exportTo(this, options, function(image) {
+      var endDate = new Date().getTime() / 1000;
+      console.log(endDate - startDate);
+    });
+
+  });
+}
+
+
+
+
+
 
