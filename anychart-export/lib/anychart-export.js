25bd383ef07da0cdf1c2e7a7e50b406fafde0c07
blackart <blackart.ru@gmail.com>
2016-12-13 17:50:03 +0800

    work commit

--- anychart-export/lib/anychart-export.js
+++ anychart-export/lib/anychart-export.js
@@ -1,37 +1,55 @@
-var fs = require('fs');
-// var jsdom = require('jsdom').jsdom;
-var opentype = require('opentype.js');
-var spawnSync = require('child_process').spawnSync;
-var extend = require('util')._extend;
+// (function(anychart, factory) {
+//   if (typeof module === 'object' && typeof module.exports === 'object') {
+//     if (typeof anychart.getGlobal == 'function') {
+//       factory.call(this, anychart);
+//     } else {
+//       module.exports = function(a) {
+//         if (typeof a.getGlobal != 'function') {
+//           throw new Error('anychart-export requires a anychart');
+//         }
+//
+//         return factory.call(this, a);
+//       };
+//     }
+//   } else {
+//     factory.call(this, anychart)
+//   }
+// })(typeof anychart !== 'undefined' ? anychart : this, function(anychart) {
+//   debugger;
+//   var window = anychart.getGlobal();
+//   var document = window.document;
 
-var fonts = {};
-var document = window.document;
-var defaultChartBounds = {
-  left: 0,
-  top: 0,
-  width: 1024,
-  height: 758
-};
+
+(function () {
+  var fs = require('fs');
+  var opentype = require('opentype.js');
+  var spawnSync = require('child_process').spawnSync;
+  var spawn = require('child_process').spawn;
+  var extend = require('util')._extend;
+  // var $ = require("jquery")(window);
+
+  var fonts = {};
+
 // var im = require('imagemagick');
 // var Stream = require('stream').Stream;
-var defaultImageSettings = [
-  {
-    name: 'type',
-    value: 'png'
-  },
-  {
-    name: 'width',
-    value: 1024
-  },
-  {
-    name: 'height',
-    value: 768
-  },
-  {
-    name: 'quality',
-    value: 92
-  }
-];
+  var defaultImageSettings = [
+    {
+      name: 'type',
+      value: 'png'
+    },
+    {
+      name: 'width',
+      value: 1024
+    },
+    {
+      name: 'height',
+      value: 768
+    },
+    {
+      name: 'quality',
+      value: 92
+    }
+  ];
 // var Inkscape = require('inkscape');
 // var svink = require('svink').svink;
 
@@ -40,239 +58,242 @@ var defaultImageSettings = [
 // var window = document.defaultView;
 
 // var anychart = require('anychart')(window);
-// var $ = require("jquery");
-// $(window);
 
-
-(function(global, factory) {
-  if (typeof module === 'object' && typeof module.exports === 'object') {
-    if (global.document) {
-      factory.call(global);
-    } else {
-      module.exports = function(w) {
-        if (!w.document) {
-          throw new Error('anychart-export requires a window with a document');
-        }
-        window = w;
-        document = w.document;
-
-        factory.call(w);
-
-        return w.anychart;
-      };
-    }
-  } else {
-    factory.call(global)
+  function isPercent(value) {
+    var l = value.length - 1;
+    return (typeof value == 'string') && l >= 0 && value.indexOf('%', l) == l;
   }
-})(typeof window !== 'undefined' ? window : this, function() {
 
-});
+  function loadDefaultFonts(callback) {
 
-
-function loadDefaultFonts(callback) {
-
-}
-
-function loadDefaultFontsSync() {
-  var fontFilesList = fs.readdirSync(__dirname + '/../fonts');
-
-  for (var i = 0, len = fontFilesList.length; i < len; i++) {
-    var fileName = fontFilesList[i];
-    var fontName = fileName.split('.')[0];
-    if (!fonts[fontName]) {
-      fonts[fontName] = opentype.loadSync(__dirname + '/../fonts/' + fileName);
-    }
   }
-  return fonts;
-}
 
-loadDefaultFontsSync();
+  function loadDefaultFontsSync() {
+    var fontFilesList = fs.readdirSync(__dirname + '/../fonts');
 
-function anychartify() {
-  document.createElementNS = function(ns, tagName) {
-    var elem = document.createElement(tagName);
-    elem.getBBox = function() {
-      var text = elem.innerHTML;
-      var fontSize = parseFloat(elem.getAttribute('font-size'));
-      var fontFamily = elem.getAttribute('font-family');
-      var fontsArr = fontFamily.split(', ');
-
-      var font;
-      for (var i = 0, len = fontsArr.length; i < len; i++) {
-        if (font = fonts[fontsArr[i]])
-          break;
+    for (var i = 0, len = fontFilesList.length; i < len; i++) {
+      var fileName = fontFilesList[i];
+      var fontName = fileName.split('.')[0];
+      if (!fonts[fontName]) {
+        fonts[fontName] = opentype.loadSync(__dirname + '/../fonts/' + fileName);
       }
-      if (!font)
-        font = fonts['Verdana'];
+    }
+    return fonts;
+  }
 
-      var scale = 1 / font.unitsPerEm * fontSize;
+  loadDefaultFontsSync();
 
-      var top = -font.ascender * scale;
-      var height = Math.abs(top) + Math.abs(font.descender * scale);
+  function anychartify() {
+    document.createElementNS = function(ns, tagName) {
+      var elem = document.createElement(tagName);
+      elem.getBBox = function() {
+        var text = elem.innerHTML;
+        var fontSize = parseFloat(elem.getAttribute('font-size'));
+        var fontFamily = elem.getAttribute('font-family');
+        var fontsArr = fontFamily.split(', ');
 
-      var width = 0;
+        var font;
+        for (var i = 0, len = fontsArr.length; i < len; i++) {
+          if (font = fonts[fontsArr[i]])
+            break;
+        }
+        if (!font)
+          font = fonts['Verdana'];
 
-      font.forEachGlyph(text, 0, 0, fontSize, undefined, function(glyph, x, y, fontSize, options) {
-        var metrics = glyph.getMetrics();
-        metrics.xMin *= scale;
-        metrics.xMax *= scale;
-        metrics.leftSideBearing *= scale;
-        metrics.rightSideBearing *= scale;
+        var scale = 1 / font.unitsPerEm * fontSize;
 
-        width += Math.abs(metrics.xMax - metrics.xMin) + metrics.leftSideBearing + metrics.rightSideBearing
-      });
+        var top = -font.ascender * scale;
+        var height = Math.abs(top) + Math.abs(font.descender * scale);
 
-      // console.log({x: 0, y: top,  width: width, height: height});
+        var width = 0;
 
-      return {x: 0, y: top,  width: width, height: height};
+        font.forEachGlyph(text, 0, 0, fontSize, undefined, function(glyph, x, y, fontSize, options) {
+          var metrics = glyph.getMetrics();
+          metrics.xMin *= scale;
+          metrics.xMax *= scale;
+          metrics.leftSideBearing *= scale;
+          metrics.rightSideBearing *= scale;
+
+          width += Math.abs(metrics.xMax - metrics.xMin) + metrics.leftSideBearing + metrics.rightSideBearing
+        });
+
+        // console.log({x: 0, y: top,  width: width, height: height});
+
+        return {x: 0, y: top, width: width, height: height};
+      };
+      return elem;
     };
-    return elem;
-  };
-}
+  }
 
-anychartify();
+  anychartify();
 
-function exportToPng_() {
-  var $container = $('#container svg')[0];
-  var svg = $container.outerHTML;
-  var convert, buffer;
+  function exportToPng_() {
+    var $container = $('#container svg')[0];
+    var svg = $container.outerHTML;
+    var convert, buffer;
 
-  fs.writeFile('gg.svg', svg, function() {
-    console.log('Written to gg.svg');
-  });
+    fs.writeFile('gg.svg', svg, function() {
+      console.log('Written to gg.svg');
+    });
 
 // Start convert
-  convert = spawn('convert', ['svg:-', 'png:-']);
+    convert = spawn('convert', ['svg:-', 'png:-']);
 
 // Pump in the svg content
-  convert.stdin.write(svg);
-  convert.stdin.end();
+    convert.stdin.write(svg);
+    convert.stdin.end();
 
 // Write the output of convert straight to the response
-  convert.stdout.on('data', function(data) {
-    try {
-      var prevBufferLength = (buffer ? buffer.length : 0),
-          newBuffer = new Buffer(prevBufferLength + data.length);
+    convert.stdout.on('data', function(data) {
+      try {
+        var prevBufferLength = (buffer ? buffer.length : 0),
+            newBuffer = new Buffer(prevBufferLength + data.length);
 
-      if (buffer) {
-        buffer.copy(newBuffer, 0, 0);
+        if (buffer) {
+          buffer.copy(newBuffer, 0, 0);
+        }
+
+        data.copy(newBuffer, prevBufferLength, 0);
+
+        buffer = newBuffer;
+      } catch (e) {
+        console.log(e);
       }
-
-      data.copy(newBuffer, prevBufferLength, 0);
-
-      buffer = newBuffer;
-    } catch (e) {
-      console.log(e);
-    }
-  });
-
-  convert.on('exit', function(code) {
-    fs.writeFile('chart.png', buffer, function() {
-      console.log('Written to chart.png');
-      process.exit(0);
     });
-  });
-}
 
-function isFunction(value) {
-  return typeof(value) == 'function';
-}
-
-function getParams(args) {
-  var arrLength = args.length;
-  var lastArg = args[arrLength - 1];
-  var callback = isFunction(lastArg) ? lastArg : null;
-
-  var options = arrLength == 1 ? undefined : callback ? args[arrLength - 2] : lastArg;
-  var params = {};
-
-  extend(params, defaultImageSettings);
-  if (typeof options == 'string') {
-    params.type = options;
-  } else if (typeof options == 'object') {
-    extend(params, options)
+    convert.on('exit', function(code) {
+      fs.writeFile('chart.png', buffer, function() {
+        console.log('Written to chart.png');
+        process.exit(0);
+      });
+    });
   }
 
-  return params;
-}
+  function isFunction(value) {
+    return typeof(value) == 'function';
+  }
 
-function getSvg(target, params) {
-  // var bounds = target.bounds();
-  // target.bounds(bounds.left(), bounds.top(), params.width, params.height);
-  // target.container('container').draw();
+  function getParams(args) {
+    var arrLength = args.length;
+    var lastArg = args[arrLength - 1];
+    var callback = isFunction(lastArg) ? lastArg : null;
 
-  var $container = $(chart.container())[0];
-  // var svgElement = document.getElementsByTagName('svg')[0];
-  var svg = svgElement.outerHTML;
-  return svg;
-}
+    var options = arrLength == 1 ? undefined : callback ? args[arrLength - 2] : lastArg;
+    var params = {};
 
-function convertSvgToImageData(svg, params) {
-  var outputSize = '-size ' + parseFloat(params.width) + 'x' + parseFloat(params.height);
-
-  var convert = spawnSync('convert',
-      ['svg:-', params.type + ':-'],
-      {
-        input: svg
-      });
-
-  return convert.stdout;
-
-
-
-
-  // var opt = {
-  //   input: './gg.svg',
-  //   output: './graphics.png'
-  // };
-  //
-  // svink(opt);
-
-
-
-
-  // var svgToPdfConverter = new Inkscape(['--export-pdf', '--export-width=1024']);
-  //
-  // fs.writeFile('./gg.svg', svg, function() {
-  //   console.log('Written to gg.svg');
-  //   var rs = fs.createReadStream('./gg.svg');
-  //   var ws = fs.createWriteStream('./foo.pdf');
-  //
-  //   rs.on('open', function () {
-  //     rs.pipe(svgToPdfConverter).pipe(ws);
-  //
-  //     console.log('Written to foo.pdf');
-  //     process.exit(0);
-  //   });
-  // });
-}
-
-function exportTo(target, options, callback) {
-  setTimeout(function() {
-    var buffer = exportToSync(target, options);
-    if (callback) {
-      callback.call(null, buffer);
+    extend(params, defaultImageSettings);
+    if (typeof options == 'string') {
+      params.type = options;
+    } else if (typeof options == 'object') {
+      extend(params, options)
     }
-  }, 0);
-}
 
-function exportToSync(target, options) {
-  var params = getParams(arguments);
-  var svg = getSvg(target, params);
+    return params;
+  }
 
-  return params.type == 'svg' ? svg : convertSvgToImageData(svg, params);
-}
+  function getSvg(target, params) {
+    var container = target.container();
+    if (!container) {
+      console.log('Warning! Target chart has not container. Use container() method to set it.');
+      return '';
+    }
+    var bounds = target.bounds();
+    if (isPercent(bounds.left) || isPercent(bounds.top) || isPercent(bounds.width) || isPercent(bounds.height)) {
 
-function loadFont(path, callback) {
+      console.log('Warning! Bounds of chart should be set in pixels. See https://api.anychart.com/7.12.0/anychart.core.Chart#bounds how do it.');
+    }
 
-}
+    var svgElement = container.container().getElementsByTagName('svg')[0];
+    var svg = svgElement.outerHTML;
+    return svg;
+  }
 
-function loadFontSync(path, callback) {
+  function convertSvgToImageData(svg, params, callback) {
+    var convert	= spawn('convert', ['svg:-', params.type + ':-']);
+    var imageData;
 
-}
+    convert.stdin.write(svg);
+    convert.stdin.end();
+
+    convert.stdout.on('data', function(data) {
+      imageData = data;
+    });
+
+    convert.on('exit', function(code) {
+      callback.call(null, imageData);
+    });
+  }
+
+  function convertSvgToImageDataSync(svg, params) {
+    var outputSize = '-size ' + parseFloat(params.width) + 'x' + parseFloat(params.height);
+
+    var convert = spawnSync('convert',
+        ['svg:-', params.type + ':-'],
+        {
+          input: svg
+        });
+
+    return convert.stdout;
 
 
-exports.exportTo = exportTo;
-exports.exportToSync = exportToSync;
-exports.loadFont = loadFont;
-exports.loadFontSync = loadFontSync;
+    // var opt = {
+    //   input: './gg.svg',
+    //   output: './graphics.png'
+    // };
+    //
+    // svink(opt);
+
+
+    // var svgToPdfConverter = new Inkscape(['--export-pdf', '--export-width=1024']);
+    //
+    // fs.writeFile('./gg.svg', svg, function() {
+    //   console.log('Written to gg.svg');
+    //   var rs = fs.createReadStream('./gg.svg');
+    //   var ws = fs.createWriteStream('./foo.pdf');
+    //
+    //   rs.on('open', function () {
+    //     rs.pipe(svgToPdfConverter).pipe(ws);
+    //
+    //     console.log('Written to foo.pdf');
+    //     process.exit(0);
+    //   });
+    // });
+  }
+
+  function exportTo(target, options, callback) {
+    var params = getParams(arguments);
+    if (params.type == 'svg') {
+      process.nextTick(function() {
+        var svg = getSvg(target, params);
+        if (callback) {
+          callback.call(null, svg);
+        }
+      })
+    } else {
+      var svg = getSvg(target, params);
+      convertSvgToImageData(svg, params, callback);
+    }
+  }
+
+  function exportToSync(target, options) {
+    var params = getParams(arguments);
+    var svg = getSvg(target, params);
+
+    return params.type == 'svg' ? svg : convertSvgToImageDataSync(svg, params);
+  }
+
+  function loadFont(path, callback) {
+
+  }
+
+  function loadFontSync(path, callback) {
+
+  }
+
+  exports.exportTo = exportTo;
+  exports.exportToSync = exportToSync;
+  exports.loadFont = loadFont;
+  exports.loadFontSync = loadFontSync;
+
+  return exports;
+})();
--- index.js
+++ index.js
@@ -2,16 +2,22 @@ var fs = require('fs');
 var jsdom = require('jsdom').jsdom;
 // var spawnSync = require('child_process').spawnSync;
 
-var document = jsdom('<div id="container"></div>');
+var document = jsdom('<body><div id="container"></div></body>');
 var window = document.defaultView;
+
+var $ = require("jquery")(window);
 
 var anychart = require('anychart')(window);
 var anychart_export = require('./anychart-export/lib/anychart-export.js');
 
-// var chart = anychart.column([1, 2, 3, 4, 5]);
-// chart.title('canvg is a SVG parser and renderer. It takes a URL to a SVG file or the text of an SVG file, parses it in JavaScript, and renders the result on a Canvas element. The rendering speed of the examples is about as fast as native SVG.');
-// chart.bounds(0, 0, 500, 600);
-// chart.container('container').draw();
+
+// $('<div></div>').attr('id', 'container').appendTo('body');
+
+// $('body').append('<div id="container"></div>');
+  var chart = anychart.column([1, 2, 3, 4, 5]);
+  chart.title('canvg is a SVG parser and renderer. It takes a URL to a SVG file or the text of an SVG file, parses it in JavaScript, and renders the result on a Canvas element. The rendering speed of the examples is about as fast as native SVG.');
+  chart.bounds(0, 0, 500, 600);
+  chart.container('container').draw();
 
 // anychart_export.exportTo(chart, null, function(image) {
 //   fs.writeFile('./image.png', image, function() {
@@ -20,14 +26,23 @@ var anychart_export = require('./anychart-export/lib/anychart-export.js');
 //   });
 // });
 
-var options = {
-  type: 'svg',
-  width: 1000,
-  height: 500
-};
+  var options = {
+    type: 'png',
+    width: 1000,
+    height: 500
+  };
 
-// var svg = anychart_export.exportToSync(chart, 'svg');
-// console.log(svg);
+  // var svg = anychart_export.exportToSync(chart, 'svg');
+  anychart_export.exportTo(chart, options.type, function(svg) {
+    fs.writeFile('./image.' + options.type, svg, function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Written to file');
+      }
+      process.exit(0);
+    });
+  });
 
 // var image = anychart_export.exportToSync(chart, options);
 //
@@ -45,24 +60,27 @@ var options = {
 //   // process.exit(0);
 // });
 
-var startDate = new Date().getTime() / 1000;
-for (var i = 0, len = 1; i < len; i++) {
-  var chart = anychart.column([1, 2, 3, 4, 5]);
-  chart.title('canvg is a SVG parser and renderer. It takes a URL to a SVG file or the text of an SVG file, parses it in JavaScript, and renders the result on a Canvas element. The rendering speed of the examples is about as fast as native SVG.');
-  chart.bounds(0, 0, 500, 600);
-  chart.container('container').draw();
-
-  console.log(chart.container())
-
-  chart.listen(anychart.enums.EventType.CHART_DRAW, function(e) {
-
-    anychart_export.exportTo(this, options, function(image) {
-      var endDate = new Date().getTime() / 1000;
-      console.log(endDate - startDate);
-    });
-
-  });
-}
+// var startDate = new Date().getTime() / 1000;
+// for (var i = 0, len = 1; i < len; i++) {
+//   var $container = $('#container' + i);
+//   $('body').append($container);
+//
+//   var chart = anychart.column([1, 2, 3, 4, 5]);
+//   chart.title('canvg is a SVG parser and renderer. It takes a URL to a SVG file or the text of an SVG file, parses it in JavaScript, and renders the result on a Canvas element. The rendering speed of the examples is about as fast as native SVG.');
+//   chart.bounds(0, 0, 500, 600);
+//   chart.container('container' + i).draw();
+//
+//   console.log(chart.container());
+//
+//   chart.listen(anychart.enums.EventType.CHART_DRAW, function(e) {
+//
+//     anychart_export.exportTo(this, options, function(image) {
+//       var endDate = new Date().getTime() / 1000;
+//       console.log(endDate - startDate);
+//     });
+//
+//   });
+// }
 
 
 
